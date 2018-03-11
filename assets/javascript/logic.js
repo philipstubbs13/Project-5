ae4201502d8db02079cc05760ab5959e00e71da1
Phil S <philipstubbs13@gmail.com>
2018-03-11 15:30:41 -0500

    Added About me section with accordion.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,14 +1,7 @@
-//  $(document).ready(function(){
-//     $('.scrollspy').scrollSpy();
-//   });
-
-//  //Initialize sidebar
-// // Initialize collapse button
-// $(".button-collapse").sideNav();
-// // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-// $('.collapsible').collapsible();
-//   // Show sideNav
-//   $('.button-collapse').sideNav('show');
+//Accordeon toggle
+$('.collapse').collapse({
+    toggle: false
+})
 
 
         
