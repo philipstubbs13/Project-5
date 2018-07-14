6685448c037a4e4908d387935dd8468b1ecbc626
philipstubbs13 <philipstubbs13@gmail.com>
2018-07-14 16:50:58 -0500

    added scroll reveal to portfolio page

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -1,7 +1,7 @@
-//Accordeon toggle
-$('.collapse').collapse({
-    toggle: false
-})
+// scroll reveal effect for portfolio page
+window.sr = ScrollReveal();
+sr.reveal('.project-card', { duration: 1000, origin: 'bottom', distance: '80px', rotate: { x: 180, y: 180, z: 180 }, easing: 'ease' });
+sr.reveal('.bar');
 
 
         
