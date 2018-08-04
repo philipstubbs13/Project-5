0d3fcbb8350f7cbc2f062d7b53f559fd6f59564e
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-04 09:18:23 -0500

    updated layout of login page

--- public/assets/javascript/auth.js
+++ public/assets/javascript/auth.js
@@ -41,45 +41,45 @@ function testAPI(){
     FB.api('/me?fields=name,email,birthday,location', function(response){
         if(response && !response.error){
             console.log(response);
-            buildProfile(response);
+            // buildProfile(response);
         }
 
         FB.api('/me/feed', function(response){
             if(response && !response.error){
-                buildFeed(response);
+                // buildFeed(response);
             }
         });
     })
 }
 
-function buildProfile(user){
-    let profile = `
-        <h3>${user.name}</h3>
-        <ul class="list-group">
-            <li class="list-group-item">User ID: ${user.id}</li>
-            <li class="list-group-item">Email: ${user.email}</li>
-            <li class="list-group-item">Birthday: ${user.birthday}</li>
-            <li class="list-group-item">Location: ${user.location.name}</li>
-        </ul>
-    `;
+// function buildProfile(user){
+//     let profile = `
+//         <h3>${user.name}</h3>
+//         <ul class="list-group">
+//             <li class="list-group-item">User ID: ${user.id}</li>
+//             <li class="list-group-item">Email: ${user.email}</li>
+//             <li class="list-group-item">Birthday: ${user.birthday}</li>
+//             <li class="list-group-item">Location: ${user.location.name}</li>
+//         </ul>
+//     `;
 
-    document.getElementById('profile').innerHTML = profile;
-}
+//     document.getElementById('profile').innerHTML = profile;
+// }
 
-function buildFeed(feed){
-    let output = '<h3>Latest Posts</h3>';
-    for(let i in feed.data){
-        if(feed.data[i].message){
-            output += `
-                <div class="card">
-                    ${feed.data[i].message} <span>${feed.data[i].created_time}</span
-                </div>
-            `;
-        }
-    }
+// function buildFeed(feed){
+//     let output = '<h3>Latest Posts</h3>';
+//     for(let i in feed.data){
+//         if(feed.data[i].message){
+//             output += `
+//                 <div class="card">
+//                     ${feed.data[i].message} <span>${feed.data[i].created_time}</span
+//                 </div>
+//             `;
+//         }
+//     }
 
-    document.getElementById('feed').innerHTML = output;
-}
+//     document.getElementById('feed').innerHTML = output;
+// }
 
 const authContent = document.getElementsByClassName('authenticated');
 const unauthContent = document.getElementsByClassName('not-authenticated');
@@ -87,8 +87,8 @@ const unauthContent = document.getElementsByClassName('not-authenticated');
 function setElements(isLoggedIn){
     if(isLoggedIn){
         document.getElementById('logout').style.display = 'block';
-        document.getElementById('profile').style.display = 'block';
-        document.getElementById('feed').style.display = 'block';
+        // document.getElementById('profile').style.display = 'block';
+        // document.getElementById('feed').style.display = 'block';
         document.getElementById('fb-btn').style.display = 'none';
         document.getElementById('heading').style.display = 'none';
         for (i = 0; i < authContent.length; i++) {
@@ -100,8 +100,8 @@ function setElements(isLoggedIn){
 
     } else {
         document.getElementById('logout').style.display = 'none';
-        document.getElementById('profile').style.display = 'none';
-        document.getElementById('feed').style.display = 'none';
+        // document.getElementById('profile').style.display = 'none';
+        // document.getElementById('feed').style.display = 'none';
         document.getElementById('fb-btn').style.display = 'block';
         document.getElementById('heading').style.display = 'block';
         for (i = 0; i < authContent.length; i++) {
