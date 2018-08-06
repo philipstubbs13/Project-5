2cbb6a9bfc2fb998d8fb76fa5ebd4d71c237489a
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-05 21:59:02 -0500

    removed rotate from scroll reveal js

--- public/assets/javascript/scroll-logic.js
+++ public/assets/javascript/scroll-logic.js
@@ -2,7 +2,7 @@
 window.sr = ScrollReveal();
 
 // scroll reveal effect for portfolio page
-sr.reveal('.project-card', { duration: 1800, origin: 'bottom', distance: '80px', rotate: { x: 180, y: 180, z: 180 }, easing: 'ease-in-out', delay: 300 });
+sr.reveal('.project-card', { duration: 1800, origin: 'bottom', distance: '80px', easing: 'ease-in-out', delay: 300 });
 
 // scroll reveal effect for elements on the left side of the page.
 sr.reveal('.page-left', {duration: 2000, origin: 'left', distance: '50%', useDelay: 'always', easing: 'ease-in-out', delay: 300, });
@@ -11,4 +11,4 @@ sr.reveal('.page-left', {duration: 2000, origin: 'left', distance: '50%', useDel
 sr.reveal('.page-right', {duration: 2000, origin: 'right', distance: '50%', useDelay: 'always', easing: 'ease-in-out', delay: 300, });
 
 // scroll reveal effect for writing projects 
-sr.reveal('.writing-type', { duration: 1800, origin: 'bottom', distance: '80px', rotate: { x: 180, y: 180, z: 180 }, easing: 'ease-in-out', delay: 300 });
+sr.reveal('.writing-type', { duration: 1800, origin: 'bottom', distance: '80px', easing: 'ease-in-out', delay: 300 });
